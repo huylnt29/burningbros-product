4ddbdc60c3c1c9071d52789f3e99123cd127717f
huylnt29 <lnthuy29012002@gmail.com>
2024-07-25 21:00:14 +0700

    [REFACTOR] Defined API call for getting products

---
+++ lib/core/data/model/paginated_product.dart
@@ -0,0 +1,23 @@
+import 'package:burningbros_product/core/data/model/product.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'paginated_product.g.dart';
+
+@JsonSerializable()
+class PaginatedProduct {
+  PaginatedProduct({
+    this.products,
+    this.total,
+    this.skip,
+    this.limit,
+  });
+
+  factory PaginatedProduct.fromJson(Map<String, Object?> json) =>
+      _$PaginatedProductFromJson(json);
+  Map<String, dynamic> toJson() => _$PaginatedProductToJson(this);
+
+  List<Product>? products;
+  int? total;
+  int? skip;
+  int? limit;
+}
