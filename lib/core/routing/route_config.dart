112004d4b0e2bcadb1bcb0f3875c996a64e33cf7
huylnt29 <lnthuy29012002@gmail.com>
2024-07-25 19:51:22 +0700

    [REFACTOR] Implemented routing core

---
+++ lib/core/routing/route_config.dart
@@ -0,0 +1,43 @@
+import 'package:burningbros_product/core/routing/route_handler.dart';
+import 'package:burningbros_product/core/routing/route_path.dart';
+import 'package:fluro/fluro.dart';
+import 'package:flutter/widgets.dart';
+
+class Routes {
+  Routes();
+
+  static final router = FluroRouter();
+  static RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
+
+  static void configureRoutes() {
+    setRouter(
+      RoutePath.splash,
+      handler: splashHandler,
+    );
+    setRouter(
+      RoutePath.products,
+      handler: viewProductsHandler,
+    );
+    setRouter(
+      RoutePath.favaroiteProducts,
+      handler: viewFavoriteProductsHandler,
+    );
+    setRouter(
+      RoutePath.searchForProducts,
+      handler: searchForProductsHandler,
+    );
+  }
+
+  static void setRouter(
+    String path, {
+    required Handler handler,
+    TransitionType? transitionType,
+  }) {
+    transitionType ??= TransitionType.cupertino;
+    router.define(
+      path,
+      handler: handler,
+      transitionType: transitionType,
+    );
+  }
+}
